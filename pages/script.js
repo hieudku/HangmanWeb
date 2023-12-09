633fd918e7ff77558448e780ad5708b629823c3c
Huba <hieu.dcu@gmail.com>
2023-12-10 11:42:44 +1300

    Updated css, html
    Added selection buttons.

--- pages/script.js
+++ pages/script.js
@@ -1,6 +1,8 @@
 const startBtn = document.getElementById("start-btn");
 startBtn.addEventListener("click", (event) => {
     let mainContent = document.getElementById("main-content");
+    let title = document.getElementById("title");
+    title.style.display = "none";
     mainContent.style.display = "flex";
     startBtn.style.display = "none";
     body.style.display = "flex";
