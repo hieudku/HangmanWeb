b451a73e607cd49db135f7e4909bdf56c07eb0b7
Huba <hieu.dcu@gmail.com>
2023-12-10 12:44:30 +1300

    Updated .js
    Added conditions and events to selection buttons.

--- pages/script.js
+++ pages/script.js
@@ -1,10 +1,52 @@
 const startBtn = document.getElementById("start-btn");
 startBtn.addEventListener("click", (event) => {
-    let mainContent = document.getElementById("main-content");
+
+    let selectText = document.getElementById("selectText");
     let title = document.getElementById("title");
+    let selection = document.getElementById("select-btn");
+
     title.style.display = "none";
-    mainContent.style.display = "flex";
     startBtn.style.display = "none";
-    body.style.display = "flex";
+    selection.style.display = "flex";
+    selectText.style.display = "block";
     
+});
+
+const animalArr = ["hippopotamus", "seahorse", "antellop", "sparrow"];
+const scienceArr = ["jupiter", "photosynthesis", "precipitation", "oxidation"]
+const activitiesArr = ["bouldering", "football", "swimming", "camping"];
+const natureArr = ["volcano", "earthquake", "hurricane", "atmosphere"];
+
+const selectTopic = document.querySelectorAll(".selectBtn");
+selectTopic.forEach(function(e) {
+    e.addEventListener("click", (event) => {
+        
+        
+        
+        let toGreen = document.getElementById("body");
+        let hideSelection = document.getElementById("select-btn");
+        
+        
+        
+        hideSelection.style.display = "none";
+
+
+
+        if (e.innerHTML == "Animals") {
+            let toPink = document.getElementById("body");
+            toPink.style.backgroundColor = "#FFDFF5";
+        }
+        else if (e.innerHTML == "Science") {
+            let toBlue = document.getElementById("body");
+            toBlue.style.backgroundColor = "#DFE6F3";
+        }
+        else if (e.innerHTML == "Activities") {
+            let toOrange = document.getElementById("body");
+            toOrange.style.backgroundColor = "#FFE4CA";
+        }
+        else if (e.innerHTML == "Nature") {
+            let toGreen = document.getElementById("body");
+            toGreen.style.backgroundColor = "#D7FFE2";
+        }
+    })  
 });
