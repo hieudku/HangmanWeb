2561ef93052d1bfe46940c02e9b7d106d5424f1f
Huba <hieu.dcu@gmail.com>
2023-12-10 13:01:34 +1300

    Updated .js
    Added function to get a random word.
    Added comments.

--- pages/script.js
+++ pages/script.js
@@ -1,3 +1,6 @@
+
+
+/*  */
 const startBtn = document.getElementById("start-btn");
 startBtn.addEventListener("click", (event) => {
 
@@ -12,41 +15,63 @@ startBtn.addEventListener("click", (event) => {
     
 });
 
+/* Delare and assign arrays of words available */
 const animalArr = ["hippopotamus", "seahorse", "antellop", "sparrow"];
 const scienceArr = ["jupiter", "photosynthesis", "precipitation", "oxidation"]
 const activitiesArr = ["bouldering", "football", "swimming", "camping"];
 const natureArr = ["volcano", "earthquake", "hurricane", "atmosphere"];
 
 const selectTopic = document.querySelectorAll(".selectBtn");
-selectTopic.forEach(function(e) {
+selectTopic.forEach(function(e) { // Loop to add events to each selection button.
     e.addEventListener("click", (event) => {
         
-        
-        
-        let toGreen = document.getElementById("body");
         let hideSelection = document.getElementById("select-btn");
-        
-        
         
         hideSelection.style.display = "none";
 
-
-
+        /* Conditions when select/click on each topic */
         if (e.innerHTML == "Animals") {
+            /* Change background color */
             let toPink = document.getElementById("body");
             toPink.style.backgroundColor = "#FFDFF5";
+
+            /* Call function to get a random word */
+            let getRandomWord = randomWord(animalArr);
+            console.log(String(getRandomWord));
         }
         else if (e.innerHTML == "Science") {
+            /* Change background color */
             let toBlue = document.getElementById("body");
             toBlue.style.backgroundColor = "#DFE6F3";
+
+            /* Call function to get a random word */
+            let getRandomWord = randomWord(scienceArr);
+            console.log(String(getRandomWord));
         }
         else if (e.innerHTML == "Activities") {
+            /* Change background color */
             let toOrange = document.getElementById("body");
             toOrange.style.backgroundColor = "#FFE4CA";
+
+            /* Call function to get a random word */
+            let getRandomWord = randomWord(activitiesArr);
+            console.log(String(getRandomWord));
         }
         else if (e.innerHTML == "Nature") {
+            /* Change background color */
             let toGreen = document.getElementById("body");
             toGreen.style.backgroundColor = "#D7FFE2";
+
+            /* Call function to get a random word */
+            let getRandomWord = randomWord(natureArr);  
+            console.log(String(getRandomWord));
         }
     })  
 });
+
+/* Get random word from each array */
+function randomWord(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    const randomWord = array[randomIndex];
+    return randomWord;
+}
